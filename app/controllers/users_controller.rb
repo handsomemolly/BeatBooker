f446016530cf52cc8842019ec7157ae5228231e2
handsomemolly <ryan.kendig@gmail.com>
2021-02-25 14:26:38 -0800

    JWT in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
-    def show
-        user = User.find_by(id: params[:id])
-        render json: user
+    skip_before_action :authorized
+
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
 
     def create
-        user = User.create(user_params)
-        render json: user
+        @user = User.create(user_params)
+        if @user.valid?
+          @token = encode_token({ user_id: @user.id })
+          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+          render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
     end
 
-    def login
-        user = User.find_by(email: user_params[:email], password: user_params[:password])
-        render json: user
+    def render_request
+        get = User.get_request
+        render json: {data: get}
     end
 
 
